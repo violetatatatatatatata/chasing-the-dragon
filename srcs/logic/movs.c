8fed875398b77e31ccdbab9a6789b1afe7aee424
Violeta desde 42 <avelandr@cbr8s5.42barcelona.com>
2026-01-29 17:43:41 +0100

    creo que está bien :p

--- srcs/logic/movs.c
+++ srcs/logic/movs.c
@@ -12,37 +12,44 @@
 
 #include <ctd.h>
 
-int move_player(t_map *m, double new_x, double new_y)
+int	move_player(t_game *game, double new_x, double new_y)
 {
 	char	next_cell;
+	int		grid_x;
+	int		grid_y;
 
-	next_cell = m->map[new_y][new_x];
+	grid_x = (int)new_x;
+	grid_y = (int)new_y;
+	next_cell = game->map.map[grid_y][grid_x];
 	if (next_cell == WALL)
 		return (0);
-	map.x_pos = new_x;
-	map.y_pos = new_y;
+	game->p.x_pos = new_x;
+	game->p.y_pos = new_y;
 	return (1);
 }
 
 int handle_key_press(int keycode, t_game *game)
 {
 	int		input;
+	double	speed;
 	double	mov;
 
-	input = 0;
-	if (keycode == ESC)
-		return (exit_game(game));
-	else if (keycode == SPA)
-		input = hero_shoot(game);
-	else if (keycode == W || keycode == A)
-		mov += 0.01;
+	speed = 0.1;
+	input = 0.0;
+	mov = 0.0;
+//	if (keycode == ESC)
+//		return (exit_game(game));
+//	else if (keycode == SPA)
+//		input = hero_shoot(game);
+	if (keycode == W || keycode == A)
+		mov += speed;
 	else if (keycode == D || keycode == S)
-		mov -= 0.01;
+		mov -= speed;
 	if (keycode == W || keycode == D)
-		input = move_player(game->map, game->map.x_pos, mov);
+		input = move_player(game, game->p.x_pos, mov);
 	else if (keycode == S || keycode == A)
-		input = move_player(game->map, mov, game->map.y_pos);
-	if (input)
-		render_map();
+		input = move_player(game, mov, game->p.y_pos);
+//	if (input)
+//		render_map();
 	return (input);
 }
